9a490e6625437ea6852afe813be91ed0fc9df735
chibao99 <chibao9965@gmail.com>
2022-04-12 13:26:55 +0700

    ++update field

--- admin/src/pages/IndexView/form-schema.js
+++ admin/src/pages/IndexView/form-schema.js
@@ -4,7 +4,7 @@ import { translatedErrors } from "@strapi/helper-plugin";
 const schema = yup.object().shape({
   title: yup.string(translatedErrors.string).nullable().required(),
   slug: yup.string(translatedErrors.string).nullable().required(),
-  order: yup.string(translatedErrors.string).nullable.required(),
+  order: yup.string(translatedErrors.string).nullable().required(),
 });
 
 export default schema;
